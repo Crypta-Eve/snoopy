b68ba68e06b78e3c18eb41aea1ed16d9c55d9cb7
Crypta Eve <crypta@crypta.tech>
2021-01-21 10:29:11 +1100

    - move DB transactions out of http handler - implement scaled workers

--- snoopy.go
+++ snoopy.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/middleware"
-	"gorm.io/gorm/logger"
 	"hash/crc32"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -20,12 +18,17 @@ import (
 )
 
 type Record struct {
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time `gorm:"index"`
-	UpdatedAt time.Time `gorm:"index"`
+	ID        uint           `gorm:"primarykey"`
+	CreatedAt time.Time      `gorm:"index"`
+	UpdatedAt time.Time      `gorm:"index"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 
-	IP string
+	IP   string
+	Slug string
+}
+
+type Hit struct {
+	IP   string
 	Slug string
 }
 
@@ -41,26 +44,30 @@ func main() {
 	mariaDatabase := envy.Get("MARIA_DATABASE", "snoopy")
 
 	// Set up GORM logger
-	dbLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			SlowThreshold: time.Second,
-			Colorful:      true,
-			LogLevel:      logger.Info,
-		},
-	)
+	//dbLogger := logger.New(
+	//	log.New(os.Stdout, "\r\n", log.LstdFlags),
+	//	logger.Config{
+	//		SlowThreshold: time.Second,
+	//		Colorful:      true,
+	//		LogLevel:      logger.Info,
+	//	},
+	//)
 
 	mariaConnect := mariaUser + ":" + mariaPass + "@tcp(" + mariaHost + ":" + "3306" + ")/" + mariaDatabase + "?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN: mariaConnect, // data source name
-		DefaultStringSize: 128, // default size for string fields
-		DisableDatetimePrecision: false, // disable datetime precision, which not supported before MySQL 5.6
-		DontSupportRenameIndex: true, // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn: true, // `change` when rename column, rename column not supported before MySQL 8, MariaDB
-		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
+		DSN:                       mariaConnect, // data source name
+		DefaultStringSize:         128,          // default size for string fields
+		DisableDatetimePrecision:  false,        // disable datetime precision, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,         // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,         // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,        // auto configure based on currently MySQL version
 	}), &gorm.Config{
-		Logger: dbLogger,
+		//Logger: dbLogger,
 	})
+	defer func() {
+		dbConn, _ := db.DB()
+		dbConn.Close()
+	}()
 
 	if err != nil {
 		log.Fatalf("failed to connect GORM to mysqldb: %s", err.Error())
@@ -80,17 +87,25 @@ func main() {
 	}
 	envTimeRaw := envy.Get("SESS_TIME", "10")
 	envTime, err := strconv.Atoi(envTimeRaw)
-	if err != nil{
+	if err != nil {
 		envTime = 10
 	}
 
+	log.Println("Setting up worker")
+
+	hitlog := make(chan Hit, 500)
+	for i := 0; i < 3; i++ {
+		go worker(i, hitlog, salt, db, envTime)
+	}
+
+	log.Println("Starting router")
 
 	r := chi.NewRouter()
 
 	// A good base middleware stack
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	//r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Use(middleware.Heartbeat("/ping"))
@@ -104,37 +119,50 @@ func main() {
 	r.Get("/snoopy/{slug}", func(w http.ResponseWriter, r *http.Request) {
 
 		ip := strings.Split(r.RemoteAddr, ":")[0]
-		log.Println(ip)
-		key := fmt.Sprint(crc32.ChecksumIEEE([]byte(ip + salt)))
 		slug := chi.URLParam(r, "slug")
 
-		rec := Record{}
-
-		res := db.First(&rec, "ip = ? AND slug = ? AND updated_at > ?", key, slug, time.Now().Add(time.Duration(-1 * envTime) * time.Minute))
-		if res.Error == gorm.ErrRecordNotFound {
-			// Need new record
-			rec = Record{
-				IP:        key,
-				Slug:      slug,
-			}
-
-			result := db.Create(&rec)
-			if result.Error != nil {
-				log.Fatalf("failed to create new record: %s", err)
-			}
-
-		} else if err != nil {
-			log.Fatalf("query error: %s", err.Error())
-		} else {
-			// Need to update the existing records last touched time
-			db.Save(&rec)
+		ht := Hit{
+			IP:   ip,
+			Slug: slug,
 		}
 
+		hitlog <- ht
 
 		w.Write([]byte(".snoopy {\n  color: #28a745 !important;\n}"))
 		w.Header().Set("Content-Type", "text/css")
 	})
 
-
 	http.ListenAndServe(":3000", r)
+	close(hitlog)
+
+	time.Sleep(time.Second)
+}
+
+func worker(id int, hits <-chan Hit, salt string, db *gorm.DB, envTime int) {
+	for hit := range hits {
+
+		key := fmt.Sprint(crc32.ChecksumIEEE([]byte(hit.IP + salt)))
+
+		rec := Record{}
+
+		res := db.First(&rec, "ip = ? AND slug = ? AND updated_at > ?", key, hit.Slug, time.Now().Add(time.Duration(-1*envTime)*time.Minute))
+		if res.Error == gorm.ErrRecordNotFound {
+			// Need new record
+			rec = Record{
+				IP:   key,
+				Slug: hit.Slug,
+			}
+
+			result := db.Create(&rec)
+			if result.Error != nil {
+				log.Fatalf("failed to create new record: %s", result.Error)
+			}
+
+		} else if res.Error != nil {
+			log.Fatalf("query error: %s", res.Error.Error())
+		} else {
+			// Need to update the existing records last touched time
+			db.Save(&rec)
+		}
+	}
 }
